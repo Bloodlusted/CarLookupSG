b7a98bb0cfdfd31f50ab5cc67df7ed82b8c56e4a
Bloodlusted <131930548+Bloodlusted@users.noreply.github.com>
2024-04-25 07:15:03 +0800

    Input validation

--- carlookup.py
+++ carlookup.py
@@ -1,3 +1,4 @@
+import re
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -5,39 +6,53 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
-# Set up Selenium WebDriver in headless mode
-chrome_options = Options()
-chrome_options.add_argument("--headless=new")
-driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://vrl.lta.gov.sg/lta/vrl/action/pubfunc?ID=EnquireRoadTaxExpDtProxy")
+def input_validation(plate_number):
+    pattern = r'^[A-Z]{1,3}\d{1,4}[A-Z]$'
+    
+    if re.match(pattern, plate_number):
+        return True
+    else:
+        return False
 
-# Fill in the vehicle number input field
-vehicle_number = input("Enter vehicle number: ")
-vehicle_input = driver.find_element(By.NAME, "vehicleNo")
-vehicle_input.send_keys(vehicle_number)
+def lookup(vehicle_number):
+    # Set up Selenium WebDriver in headless mode
+    chrome_options = Options()
+    chrome_options.add_argument("--headless=new")
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get("https://vrl.lta.gov.sg/lta/vrl/action/pubfunc?ID=EnquireRoadTaxExpDtProxy")
 
-# Click the "I agree" checkbox
-agree_checkbox = driver.find_element(By.NAME, "agreeTC")
-agree_checkbox.click()
+    # Find and fill in the vehicle number input field
+    vehicle_input = driver.find_element(By.NAME, "vehicleNo")
+    vehicle_input.send_keys(vehicle_number)
 
-# Click the "Next" button
-next_button = driver.find_element(By.ID, "btnNext")
-next_button.click()
+    # Find and click the "I agree" checkbox
+    agree_checkbox = driver.find_element(By.NAME, "agreeTC")
+    agree_checkbox.click()
 
-try:
-    # Wait for the page to load before extracting
-    wait = WebDriverWait(driver, 5)
-    wait.until(EC.presence_of_element_located((By.CLASS_NAME, "dt-payment-dtls")))
+    # Find and click the "Next" button
+    next_button = driver.find_element(By.ID, "btnNext")
+    next_button.click()
 
-    # Extract required information
-    model = driver.find_element(By.XPATH, "//div[@class='dt-payment-dtls']//div[contains(@class, 'col-xs-5')]//p").text
-    expiry = driver.find_element(By.XPATH, "//p[contains(@class, 'vrlDT-content-p')]").text
-    print("Vehicle Make/Model:", model)
-    print("Road Tax Expiry Date:", expiry)
+    try:
+        # Wait for the page to load before extracting
+        wait = WebDriverWait(driver, 5)
+        wait.until(EC.presence_of_element_located((By.CLASS_NAME, "dt-payment-dtls")))
 
-except TimeoutException:
-    print("No record found")
-except Exception as e:
-    print("An error occurred:", e)
+        # Extract required information
+        model = driver.find_element(By.XPATH, "//div[@class='dt-payment-dtls']//div[contains(@class, 'col-xs-5')]//p").text
+        expiry = driver.find_element(By.XPATH, "//p[contains(@class, 'vrlDT-content-p')]").text
+        print("Vehicle Make/Model:", model)
+        print("Road Tax Expiry Date:", expiry)
 
-driver.quit()
+    except TimeoutException:
+        print("No record found")
+    except Exception as e:
+        print("An error occurred:", e)
+
+    driver.quit()
+
+query = input("Enter the vehicle plate number: ")
+if input_validation(query) == False:
+    print("Error: Please input a valid license plate")
+else:
+    lookup(query)
